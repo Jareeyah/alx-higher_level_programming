a585efdd8a4c55b7c80c213f8ba875ab3f7f2813
Jareeyah <jariatdosunmu@gmail.com>
2023-08-28 18:52:19 -0700

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,5 +11,5 @@ def safe_print_list_integers(my_list=[], x=0):
             s = s + 1
         except (ValueError, TypeError):
             continue
-        print()
-        return s
+    print()
+    return s
