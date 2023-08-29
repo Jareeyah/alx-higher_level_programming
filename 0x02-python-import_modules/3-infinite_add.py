25e3c8b49f55c735ce781f888e7fde84d76b085d
Jareeyah <jariatdosunmu@gmail.com>
2023-08-28 19:46:22 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,5 +6,4 @@ if (__name__ == "__main__"):
     args = 0
     for i in range(len(sys.argv) - 1):
         args += int(sys.argv[i + 1])
-
-    print("{}".format(args))
+        print("{}".format(args))
