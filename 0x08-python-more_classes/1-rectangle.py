a33bcb03db9b1b8c377f9e4a494730b493629a87
Jariat  Dosunmu <125503787+Jareeyah@users.noreply.github.com>
2023-09-05 03:05:55 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """A rectngle class based on 0-rectangle.py"""
+    
     def __init__(self, width=0, height=0):
         """Initializing a new rectangle"""
         self.height = height
